3f9380b38461355ffffaa8eaa6da743d2b6c8eda
brenobreves <brenocsb@dcc.ufrj.br>
2023-04-09 20:08:41 -0300

    Ajuste na virada das cartas e teste de embaralhamento

--- script.js
+++ script.js
@@ -15,24 +15,35 @@ while(qtdcartas > 14 || qtdcartas < 4 || qtdcartas%2 == 1){
     qtdcartas = prompt("Atenção! O número de cartas deve ser um numero par e estar entre 4 e 14!")
 }
 
-embaralhar = gifs.slice(0,qtdcartas)
-
+embaralhar = gifs.slice(0,qtdcartas);
+alert(embaralhar);
+embaralhar.sort(comparador);
+alert(embaralhar);
 const cardList = document.querySelector('ul')
 for(let i = 0 ; i < qtdcartas ; i++){
     cardList.innerHTML += "<li><div onclick='virar(this)' class='card'><div class='front-face face'><img src='./imagens/back.png'></div><div class='back-face face'></div></div></li>"
 }
 function virar(p){
-    carta1 = p.querySelector(".front-face");
-    carta1.classList.toggle("front");
-    carta2 = p.querySelector(".back-face");
-    carta2.classList.toggle("back");
-    contviradas++
-    if(contviradas == 1){
+    if(contviradas==0){
+        carta1 = p.querySelector(".front-face");
+        carta1.classList.toggle("front");
+        carta2 = p.querySelector(".back-face");
+        carta2.classList.toggle("back");
+        contviradas++
+        jogadas++
         Pcarta = p;
+    }    
+    if(contviradas == 1 && Pcarta != p){
+        carta1 = p.querySelector(".front-face");
+        carta1.classList.toggle("front");
+        carta2 = p.querySelector(".back-face");
+        carta2.classList.toggle("back");
+        contviradas++
+        jogadas++
+        Scarta = p;
     }
     if(contviradas == 2){
-        contviradas = 0
-        Scarta = p
+        contviradas++
         setTimeout(reset,1000);
     }
 }
@@ -45,4 +56,8 @@ function reset(){
     carta1.classList.toggle("front");
     carta2 = Scarta.querySelector(".back-face");
     carta2.classList.toggle("back");
+    contviradas = 0
+}
+function comparador() { 
+	return Math.random() - 0.5; 
 }
